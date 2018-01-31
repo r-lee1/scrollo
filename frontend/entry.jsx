b26ebdeae41879cb96c6d642cdddba885b3398c1
r-lee1 <13248883+r-lee1@users.noreply.github.com>
2018-01-31 10:05:05 -0800

    add root view

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,1 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById("root");
+  ReactDOM.render(<h1>Welcome to Scrollo!</h1>, root);
+});
