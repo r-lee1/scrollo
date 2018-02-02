f93ae979aebb632f305fb99a7548f21cdc689134
r-lee1 <13248883+r-lee1@users.noreply.github.com>
2018-02-01 16:15:20 -0800

    add styling for sign up page

--- frontend/components/session/signup.jsx
+++ frontend/components/session/signup.jsx
@@ -45,33 +45,37 @@ class Signup extends React.Component {
         <div className="session-form-container">
           <h1 className="logo">scrollo.</h1>
           <form className="session-form" onSubmit={this.handleSubmit}>
+            <div className="session-form-input">
+              <label>
+              <input
+                placeholder="Username"
+                onChange={this.handleInput("username")}
+                type="text"
+                value={this.state.username}></input>
+              </label>
+              <label>
+              <input
+                placeholder="Email"
+                onChange={this.handleInput("email")}
+                type="text"
+                value={this.state.email}></input>
+              </label>
+              <label>
+              <input
+                placeholder="Password"
+                onChange={this.handleInput("password")}
+                type="text"
+                value={this.state.password}></input>
+              </label>
+            </div>
             {this.renderErrors()}
-            <label>
-            <input
-              placeholder="Username"
-              onChange={this.handleInput("username")}
-              type="text"
-              value={this.state.username}></input>
-            </label>
-            <label>
-            <input
-              placeholder="Email"
-              onChange={this.handleInput("email")}
-              type="text"
-              value={this.state.email}></input>
-            </label>
-            <label>
-            <input
-              placeholder="Password"
-              onChange={this.handleInput("password")}
-              type="text"
-              value={this.state.password}></input>
-            </label>
-            <button>Create Account</button>
+            <div className="session-form-btn">
+              <button>Sign Up</button>
+            </div>
           </form>
         </div>
       </div>
       );
+    }
   }
-}
  export default Signup;
