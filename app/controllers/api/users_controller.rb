ce64615d1c34b169aaa0c6eb7a062d5d85d15dad
r-lee1 <13248883+r-lee1@users.noreply.github.com>
2018-03-15 16:17:10 -0700

    add default avatar for new users, edit styling

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,7 +2,7 @@ class Api::UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    # @user.img_url = ""
+    @user.img_url = "https://res.cloudinary.com/dxyit2phe/image/upload/v1521152604/if_Smiley_34616_ix351c.png"
     if @user.save
       login(@user)
       render "api/users/show"
